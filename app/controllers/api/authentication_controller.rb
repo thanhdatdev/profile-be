8cdeae10f7e8b3465b054b30f466a0d2a8e57f23
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2022-03-08 22:39:43 +0700

    set cookie if authenticate success

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -2,10 +2,20 @@ module Api
   class AuthenticationController < ApplicationController
     skip_before_action :authenticate_request
 
+    include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
+
+    protect_from_forgery with: :exception
+
     def authenticate
       command = AuthenticateUser.call(params[:email], params[:password])
 
       if command.success?
+        cookies['ACCESS_TOKEN'] = {
+          :value => command.result,
+          :expires => 24.hours.from_now,
+          :domain => :all
+        }
         render json: { auth_token: command.result }
       else
         render json: { error: command.errors }, status: :unauthorized
