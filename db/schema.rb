68388e660013e65ad6a3c3bcf35334db5c49a094
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2022-03-08 10:44:11 +0700

    use bcrypt for password

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_031033) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
